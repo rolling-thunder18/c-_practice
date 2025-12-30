020f4b04bcbd1163667e29646d7d86c763fc320d
rolling-thunder18 <nissanmohanty18@gmail.com>
2025-12-31 00:40:24 +0530

    okay good

--- python/gpt.py
+++ python/gpt.py
@@ -1,7 +1,7 @@
 #the string input is done here
 x=input("enter ur string: ")
 #question 1
-if x.startswith("hi" or "HI"):
+if x.startswith("hi"):
     print(True)
 else:
     print(False)
@@ -15,3 +15,46 @@ else:
 #question 3
 y=x.lower()
 print(y)
+
+#question 4
+y=x.strip()
+print(y)
+
+#question 5
+y=x.replace(" ","-")
+print(y)
+
+#question 6
+y=x
+count=0
+
+#for loop method 
+for i in y:
+    if i=="a":
+        count+=1
+
+print(f"number of a is {count} times in the sentence")
+#the method u meant me to do is it find?
+
+
+#question 7
+y=x.find("py")
+print(y)
+#well here i found out that .find() registers the last found element provided so every instance int stored
+
+#question 8
+y=x.split()
+print(y[4])
+
+#question 9 
+# hmmm thats a tough one lemme try
+
+def lol(x):
+    y=x.split()
+    y.sort()
+    result=" ".join(y)
+    return result
+
+print(lol(x))
+
+#question 10- wait i did that right
