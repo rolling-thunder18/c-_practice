0a51e68d665d4f1c19728a84222251839c3be30a
rolling-thunder18 <nissanmohanty18@gmail.com>
2025-12-31 13:34:09 +0530

    okay good

--- python/gpt.py
+++ python/gpt.py
@@ -44,7 +44,7 @@ print(y)
 
 #question 8
 y=x.split()
-print(y[4])
+print(y[0])
 
 #question 9 
 # hmmm thats a tough one lemme try
@@ -59,5 +59,43 @@ print(lol(x))
 
 #question 10- wait i did that right
 
-#question 11
+#question 11 the normal count way i did that already
+
+#question 12
+print()
+
+y=x.replace(" ","_")
+
+print(y)
+if y[0]+y[1]=="hi":
+    print(f"the sentence starts with hi")
+print(y[3])
+
+#question 13
+
+y=x.replace(" ","_")
+if y[-3]+y[-2]+y[-1]=="end":
+    print("the string ends with end")
+print(y[-1])
+#weird method but i couldnt think of anything else and using manual method to do .replace hell nah 
+
+#question 14
+
+y=x
+print(y[::-1])
+#i know this method i dont know like how to write it in python 
+print(y)
+#question 15
+#its the same as the previous on ejust check if original== reversed but the thing is i dont even know how to reverse manually so idk
+
+
+#question 16
+
+key="py"
+y=x
+for i in y:
+    if key in i:
+        print(i)
+    else:
+        print(False)
 
